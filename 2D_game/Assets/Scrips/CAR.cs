01e9a7da9684854bff2cccee91ed0a16653d8734
DeadlyCrazyFrog <37039662+DeadlyCrazyFrog@users.noreply.github.com>
2020-12-03 21:34:13 +0800

    接觸C#

---
+++ 2D_game/Assets/Scrips/CAR.cs
@@ -0,0 +1,35 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class CAR : MonoBehaviour
+{
+
+    int size;
+    float weight;
+    string colar;
+    string logo;
+    bool has_window;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int size;
+        float weight;
+        string colar;
+        string logo;
+        bool has_window;
+        size = 3;
+        weight = 300.5F;
+        colar = "white";
+        logo = "BMW";
+        has_window = false;
+    }
+}
