9e5c09b06dafafb5bf40573ca0c3774a6499b3b6
DeadlyCrazyFrog <37039662+DeadlyCrazyFrog@users.noreply.github.com>
2020-12-08 21:29:56 +0800

    程式:CARS
    資料的取得與設定

--- 2D_game/Assets/Scrips/CAR.cs
+++ 2D_game/Assets/Scrips/CAR.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class CAR : MonoBehaviour
 {
-
-    int size;
-    float weight;
-    string colar;
-    string logo;
-    bool has_window;
-    
-
-    // Start is called before the first frame update
-    void Start()
+    // [Header("字串")]  標題黨
+    // [Tooltip("字串")] 註解(游標接觸觸發)
+    // [Range( n , m )] 數值範圍 n m 為最小值及最大值(注意類別: float double)
+    [Header("車輛參數設定")]
+    public int size = 3;
+    [Tooltip("單位:頓")]
+    [Range( 0F , 1.5F )]
+    public float weight = 0.5F;
+    public string colar = "white";
+    public string logo = "BMW";
+    public bool has_window = false;
+    //===============================顏色設定
+    public Color colorA = Color.blue;
+    //自訂義(新增)要加 NEW
+    public Color colorB = new Color(); //新增自定義顏色
+    public Color colorC = new Color( 0.1f , 0.3f , 0.5f ,0.7f ); //新增自定義顏色( R, G, B, A) ,RGB值及A值(透明度)皆界在0~1
+    //===============================向量設定 VECTOR 有2-4維
+    public Vector2 V2A = Vector2.one; //vector2 是2維
+    public Vector2 V2B = Vector2.zero;
+    public Vector2 V2C = new Vector2(0.5f,0.5f);
+    //====音效片段
+    public AudioClip AudioA;
+    //====自訂義圖片(需從檔案輸入)可改變2D圖片及介面圖片
+    public Sprite SpriteA;
+    //====新增遊戲元件 物件名稱左側有立方
+    public GameObject GO_A;
+    public GameObject GO_B;
+    //====遊戲跟目錄屬性(可折疊 粗體字)有包含即可套入
+    public Transform tra_1;
+    public Camera cm_1;
+    //====開始事件 資料的取得與設定
+    private void Start()
     {
-        
+        print("Hello Kid");
+        print("品牌為"+logo);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        int size;
-        float weight;
-        string colar;
-        string logo;
-        bool has_window;
-        size = 3;
-        weight = 300.5F;
-        colar = "white";
-        logo = "BMW";
-        has_window = false;
-    }
 }
---
+++ 2D_game/Assets/Scrips/TetrisManager.cs
@@ -0,0 +1,23 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+   [Header("掉落時間")] 
+   [Range(0.1f,3f)]
+   public float Drop_Speed=1.5F;
+   [Header("目前分數")]
+   public float Your_score;// 目前分數
+   [Header("最高分數")]
+   public float highest_score;// 最高分數      
+   [Header("等級")]
+   public int Level =1;// 等級        
+   [Header("結束畫面")]
+   public GameObject result_scene;// 結束畫面      
+   [Header("音效:掉落、移動與旋轉、消除、結束")]
+   public AudioClip drop_sound;//方塊掉落音效    
+   public AudioClip rotate_sound;//方塊移動與旋轉音效 
+   public AudioClip delete_sound;//方塊消除音效    
+   public AudioClip End_sound;//遊戲結束音效    
+}
