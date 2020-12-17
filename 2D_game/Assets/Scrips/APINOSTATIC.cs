950063c65b40b384fedcd5fc0f0b8a57e337333d
DeadlyCrazyFrog <37039662+DeadlyCrazyFrog@users.noreply.github.com>
2020-12-17 21:32:38 +0800

    成功生成方塊

---
+++ 2D_game/Assets/Scrips/APINOSTATIC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APINOSTATIC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform traA;
+    public Transform traB;
+    public Transform traC;
+    public GameObject GOB_1;
+    void Start()
+    {
+        print("此物件的位置" + traA.position);
+        traB.position = new Vector3(1, 2, 65);
+        print("此物件的塗層為" + GOB_1.layer);
+        GOB_1.layer = 4;
+        print(GOB_1.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        traC.Rotate(0, 0, -1);
+        traC.Translate(0, 10, 0);
+    }
+}
--- 2D_game/Assets/Scrips/TetrisManager.cs
+++ 2D_game/Assets/Scrips/TetrisManager.cs
@@ -4,28 +4,49 @@ using UnityEngine;
 
 public class TetrisManager : MonoBehaviour
 {
-   [Header("掉落時間")] 
-   [Range(0.1f,3f)]
-   public float Drop_Speed=1.5F;
-   [Header("目前分數")]
-   public float Your_score;// 目前分數
-   [Header("最高分數")]
-   public float highest_score;// 最高分數      
-   [Header("等級")]
-   public int Level =1;// 等級        
-   [Header("結束畫面")]
-   public GameObject result_scene;// 結束畫面      
-   [Header("音效:掉落、移動與旋轉、消除、結束")]
-   public AudioClip drop_sound;//方塊掉落音效    
-   public AudioClip rotate_sound;//方塊移動與旋轉音效 
-   public AudioClip delete_sound;//方塊消除音效    
-   public AudioClip End_sound;//遊戲結束音效    
+    [Header("掉落時間")]
+    [Range(0.1f, 3f)]
+    public float Drop_Speed = 1.5F;
+    [Header("目前分數")]
+    public float Your_score;// 目前分數
+    [Header("最高分數")]
+    public float highest_score;// 最高分數      
+    [Header("等級")]
+    public int Level = 1;// 等級        
+    [Header("結束畫面")]
+    public GameObject result_scene;// 結束畫面      
+    [Header("音效:掉落、移動與旋轉、消除、結束")]
+    public AudioClip drop_sound;//方塊掉落音效    
+    public AudioClip rotate_sound;//方塊移動與旋轉音效 
+    public AudioClip delete_sound;//方塊消除音效    
+    public AudioClip End_sound;//遊戲結束音效    
+    [Header("下一個方塊")]
+    public Transform nextteris;
+    [Header("產生方塊的目標區域")]
+    public Transform to_canvas;
+    public int nexttarget;
+    private void Start()
+    {
+        generate();
+    }
+    public void StartGAME()
+    {
+        //生成generate()裡所指定之方塊樣式
+        //Instantiate(產生的子物件,子物件的目標區域);
+        GameObject teris = nextteris.GetChild(nexttarget).gameObject;
+        GameObject current=Instantiate(teris, to_canvas);
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(-84,207);
+        teris.SetActive(false);
+        generate();
+    }
     #region 方法
     /// <summary>
     /// 生成俄羅斯方塊
     /// </summary>
     private void generate()
     {
+       nexttarget = Random.Range(0, 6);
+        nextteris.GetChild(nexttarget).gameObject.SetActive(true); 
     }
     /// <summary>
     /// 添加分數
